0191091ab82b6dccab26bed59db692d21d43496e
Ali Akhtar <Ali.Akhtar@corp.wizlinx.com>
2022-08-04 19:50:48 +0500

    first commit

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Task } from '../../Task';
+import { TasksService } from '../../services/tasks.service';
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css'],
+})
+export class TasksComponent implements OnInit {
+  tasks: Task[] = [];
+  constructor(private taskService: TasksService) {}
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
+
+  deleteTask(task: Task) {
+    this.taskService
+      .deleteTasks(task)
+      .subscribe(
+        () => (this.tasks = this.tasks.filter((t) => t.id !== task.id))
+      );
+  }
+  toggleReminder(task: Task) {
+    task.reminder = !task.reminder;
+    this.taskService.updateTaskReminder(task).subscribe();
+  }
+}
